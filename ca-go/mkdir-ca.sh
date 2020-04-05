cb3ec3bb9ecb2ec54c5d19ef88b550e29b0fd2fb
sky0621 <sky14work@gmail.com>
2020-04-05 18:20:22 +0900

    ca-go

---
+++ ca-go/mkdir-ca.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+if [ $# -ne 1 ]; then
+  echo "need 1 arg" 1>&2
+  exit 1
+fi
+
+SCRIPTDIR=$(cd $(dirname $BASH_SOURCE); pwd)
+
+if [ ! -d $1 ]; then
+  echo "not directory" 1>&2
+  exit 1
+fi
+
+cd $1
+
+mkdir cmd usecase domain adapter driver
+
+(
+  cd adapter
+  mkdir -p controller/dto gateway/dto
+  cp ${SCRIPTDIR}/controller/MEMO.md controller/
+  cp ${SCRIPTDIR}/gateway/MEMO.md gateway/
+)
+
+(
+  cd cmd
+  cp ${SCRIPTDIR}/src/main.go ./
+)
+
+tree
+
